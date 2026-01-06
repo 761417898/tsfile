f45f7f7bfdb8a6320649771e50b847bd98845196
761417898 <761417898@qq.com>
2026-01-06 16:18:49 +0800

    Using the GCC built-in function __builtin_ctzll to count trailing zeros in various algorithms

--- cpp/src/encoding/encode_utils.h
+++ cpp/src/encoding/encode_utils.h
@@ -24,61 +24,11 @@
 namespace storage {
 
 FORCE_INLINE int32_t number_of_leading_zeros(int32_t i) {
-    if (i == 0) {
-        return 32;
-    }
-    int32_t n = 1;
-    if (((uint32_t)i) >> 16 == 0) {
-        n += 16;
-        i <<= 16;
-    }
-    if (((uint32_t)i) >> 24 == 0) {
-        n += 8;
-        i <<= 8;
-    }
-    if (((uint32_t)i) >> 28 == 0) {
-        n += 4;
-        i <<= 4;
-    }
-    if (((uint32_t)i) >> 30 == 0) {
-        n += 2;
-        i <<= 2;
-    }
-    n -= ((uint32_t)i) >> 31;
-    return n;
+    return i == 0 ? 32 : __builtin_clz(static_cast<uint32_t>(i));
 }
 
 FORCE_INLINE int32_t number_of_trailing_zeros(int32_t i) {
-    if (i == 0) {
-        return 32;
-    }
-    uint32_t x = static_cast<uint32_t>(i);
-    int32_t n = 31;
-    uint32_t y;
-    y = x << 16;
-    if (y != 0) {
-        n -= 16;
-        x = y;
-    }
-
-    y = x << 8;
-    if (y != 0) {
-        n -= 8;
-        x = y;
-    }
-
-    y = x << 4;
-    if (y != 0) {
-        n -= 4;
-        x = y;
-    }
-
-    y = x << 2;
-    if (y != 0) {
-        n -= 2;
-        x = y;
-    }
-    return n - static_cast<int32_t>((x << 1) >> 31);
+    return i == 0 ? 32 : __builtin_ctz(static_cast<uint32_t>(i));
 }
 
 FORCE_INLINE int get_int32_max_bit_width(const std::vector<int32_t>& nums) {
@@ -91,67 +41,11 @@ FORCE_INLINE int get_int32_max_bit_width(const std::vector<int32_t>& nums) {
 }
 
 FORCE_INLINE int32_t number_of_leading_zeros(int64_t i) {
-    if (i == 0) {
-        return 64;
-    }
-    int32_t n = 1;
-    int32_t x = (int32_t)(((uint64_t)i) >> 32);
-    if (x == 0) {
-        n += 32;
-        x = (int32_t)i;
-    }
-    if (((uint32_t)x) >> 16 == 0) {
-        n += 16;
-        x <<= 16;
-    }
-    if (((uint32_t)x) >> 24 == 0) {
-        n += 8;
-        x <<= 8;
-    }
-    if (((uint32_t)x) >> 28 == 0) {
-        n += 4;
-        x <<= 4;
-    }
-    if (((uint32_t)x) >> 30 == 0) {
-        n += 2;
-        x <<= 2;
-    }
-    n -= ((uint32_t)x) >> 31;
-    return n;
+    return i == 0 ? 64 : __builtin_clzll(static_cast<uint64_t>(i));
 }
 
 FORCE_INLINE int32_t number_of_trailing_zeros(int64_t i) {
-    if (i == 0) return 64;
-    uint32_t x, y;
-    int32_t n = 63;
-    y = static_cast<uint32_t>(i);
-    if (y != 0) {
-        n -= 32;
-        x = y;
-    } else {
-        x = static_cast<uint32_t>(static_cast<uint64_t>(i) >> 32);
-    }
-    y = x << 16;
-    if (y) {
-        n -= 16;
-        x = y;
-    }
-    y = x << 8;
-    if (y) {
-        n -= 8;
-        x = y;
-    }
-    y = x << 4;
-    if (y) {
-        n -= 4;
-        x = y;
-    }
-    y = x << 2;
-    if (y) {
-        n -= 2;
-        x = y;
-    }
-    return n - static_cast<int32_t>((x << 1) >> 31);
+    return i == 0 ? 64 : __builtin_ctzll(static_cast<uint64_t>(i));
 }
 
 FORCE_INLINE int get_int64_max_bit_width(const std::vector<int64_t>& nums) {
